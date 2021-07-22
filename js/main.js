609495d0e7bef4279ad33b656592e511e9bc3800
DSLKillerBR <breno.oliveira@hotmail.com.br>
2021-07-22 12:14:08 -0300

    Criação das páginas extras

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 //var alerta = "ESSE SITE É FICTÍCIO. NENHUM IMÓVEL ESTÁ À VENDA, E AS IMAGENS SÃO APENAS ILUSTRAÇÕES!!!!"
 
 //alert (alerta)
+function redirecionar(){
+    //window.open("https://www.cruzeiro.com.br/") //abre o link em uma nova página
+    window.location.href = "http://projetosbreno.infinityfreeapp.com/Pinheiro%20Imoveis/Site_Negociacao_Imoveis/index.html" //abre o link na mesma página
+
+}
