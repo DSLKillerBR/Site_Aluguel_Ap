3d5c58f32ae30104c5a91076ce2d90ac15580f57
breno_pinheir0 <breno.oliveira@hotmail.com.br>
2023-06-14 22:03:20 -0300

    Retomando o projeto

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
-//var alerta = "ESSE SITE É FICTÍCIO. NENHUM IMÓVEL ESTÁ À VENDA, E AS IMAGENS SÃO APENAS ILUSTRAÇÕES!!!!"
 
-//alert (alerta)
 function redirecionar(){
-    //window.open("https://www.cruzeiro.com.br/") //abre o link em uma nova página
-    window.location.href = "http://projetosbreno.infinityfreeapp.com/Pinheiro%20Imoveis/Site_Negociacao_Imoveis/index.html" //abre o link na mesma página
-
+    window.location.href = "http://projetosbreno.infinityfreeapp.com/Pinheiro%20Imoveis/Site_Negociacao_Imoveis/index.html" 
 }
